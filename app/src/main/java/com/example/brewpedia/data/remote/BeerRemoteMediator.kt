92bcc51645d0f29976f479a9d599f4f70cecac87
teotia-codes <priyanshu.teotia50@gmail.com>
2024-03-24 17:28:15 +0530

    completed with beer database and BeerRemoteMediator setup which fetches the data using api and save it in beerEntities

--- app/src/main/java/com/example/brewpedia/data/remote/BeerRemoteMediator.kt
+++ app/src/main/java/com/example/brewpedia/data/remote/BeerRemoteMediator.kt
@@ -1,4 +1,65 @@
 package com.example.brewpedia.data.remote
 
-class BeerRemoteMediator {
+import android.net.http.HttpException
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadType
+import androidx.paging.PagingState
+import androidx.paging.RemoteMediator
+import androidx.room.withTransaction
+import com.example.brewpedia.data.local.BeerDatabase
+import com.example.brewpedia.data.local.BeerEntity
+import com.example.brewpedia.data.mappers.toBeerEntity
+import java.io.IOException
+
+@OptIn(ExperimentalPagingApi::class)
+class BeerRemoteMediator(
+    private val beerDb: BeerDatabase,
+    private val beerApi: BeerApi
+): RemoteMediator<Int, BeerEntity>() {
+    @OptIn(ExperimentalPagingApi::class)
+    override suspend fun load(
+        loadType: LoadType,
+        state: PagingState<Int, BeerEntity>,
+    ): MediatorResult {
+        return try {
+            val loadKey = when(loadType) {
+                LoadType.REFRESH -> 1
+                LoadType.PREPEND -> return MediatorResult.Success(
+                    endOfPaginationReached = true
+                )
+                LoadType.APPEND -> {
+                    val lastItem = state.lastItemOrNull()
+                    if (lastItem == null){
+                        1
+                    } else{
+                        (lastItem.id / state.config.pageSize) + 1
+
+                    }
+                }
+            }
+            val beers = beerApi.getBeers(
+                page = loadKey,
+                pageCount = state.config.pageSize
+            )
+            beerDb.withTransaction {
+                if(loadType == LoadType.REFRESH) {
+                    beerDb.dao.clearALl()
+                }
+                val beerEntities = beers.map {
+                    it.toBeerEntity()
+                }
+                beerDb.dao.upsertAll(beerEntities)
+            }
+            MediatorResult.Success(
+                endOfPaginationReached = beers.isEmpty()
+            )
+
+        } catch (e: IOException) {
+            MediatorResult.Error(e)
+        } catch (e: retrofit2.HttpException) {
+            MediatorResult.Error(e)
+        }
+
+
+    }
 }
